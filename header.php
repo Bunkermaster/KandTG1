2461f9d4c009703a601357d88936fea8ab6a9821
bunkermaster <yann.le-scouarnec@hetic.net>
2017-02-28 15:41:50 +0100

    Modification du header pour class=active

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <?php
+//var_dump(pathinfo($_SERVER['PHP_SELF'])['filename']);die();
 function activeOuPas($page)
 {
-    // if?
-    // $_SERVER['PHP_SELF']
-    return ' class="active"';
+    if(pathinfo($_SERVER['PHP_SELF'])['filename'] == $page){
+        return ' class="active"';
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -22,11 +23,11 @@ function activeOuPas($page)
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li<?=activeOuPas('index.php')?>><a href="index.php">Teletubbies</a></li>
-                <li<?=activeOuPas('kittens.php')?>><a href="kittens.php">Kittens</a></li>
-                <li<?=activeOuPas('ironmaiden.php')?>><a href="ironmaiden.php">Iron Maiden</a></li>
-                <li<?=activeOuPas('16horsepower.php')?>><a href="16horsepower.php">16 Horse power</a></li>
-                <li<?=activeOuPas('manga.php')?>><a href="manga.php">Manga</a></li>
+                <li<?=activeOuPas('index')?>><a href="index.php">Teletubbies</a></li>
+                <li<?=activeOuPas('kittens')?>><a href="kittens.php">Kittens</a></li>
+                <li<?=activeOuPas('ironmaiden')?>><a href="ironmaiden.php">Iron Maiden</a></li>
+                <li<?=activeOuPas('16horsepower')?>><a href="16horsepower.php">16 Horse power</a></li>
+                <li<?=activeOuPas('manga')?>><a href="manga.php">Manga</a></li>
             </ul>
         </div>
     </div>
