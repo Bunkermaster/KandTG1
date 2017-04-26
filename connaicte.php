27c413b65533d58b95c3d212e06aef31ff82af9c
bunkermaster <yann.le-scouarnec@hetic.net>
2017-04-26 10:33:49 +0200

    MaJ connaicte.php

--- connaicte.php
+++ connaicte.php
@@ -1,4 +1,16 @@
 <?php
+function slugDejaPris(PDO $pdo, $slug)
+{
+    $sql = "SELECT count(*) as le_count FROM `page` WHERE slug = :slug";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':slug', htmlentities($slug));
+    $stmt->execute();
+    if ($stmt->errorCode() !== "00000") {
+        die($stmt->errorInfo()[2]);
+    }
+    return (bool) $stmt->fetch()[0];
+}
+
 try {
     $pdo = new PDO('mysql:dbname=kandtg1;host=localhost;port=3306','root','root');
 } catch (PDOException $exception) {
